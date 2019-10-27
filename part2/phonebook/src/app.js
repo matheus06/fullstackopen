1daf425d3cbb3654ed921c4f421820b624fb7a77
matheus-sexto <matheus.sexto@gmail.com>
2019-10-27 20:26:28 +0000

    Forms - 2.9*: The Phonebook Step4

--- part2/phonebook/src/app.js
+++ part2/phonebook/src/app.js
@@ -1,9 +1,11 @@
 
 import React, { useState } from 'react'
 
-const Numbers = ({ persons }) => {
+const Numbers = ({ persons, filter }) => {
 
-  const rows = () => persons.map((person,index) =>
+  const personsToShow =  persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+
+  const rows = () => personsToShow.map((person,index) =>
     <p key={index}> {person.name} {person.number}</p>
   )
   return (
@@ -15,10 +17,15 @@ const Numbers = ({ persons }) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -26,6 +33,10 @@ const App = () => {
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
   }
 
   const addPerson = (event) => {
@@ -49,6 +60,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+          filter shown with: <input value={filter} onChange={handleFilterChange} />
+        </div>
+      <h2>dd a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -61,7 +76,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <Numbers persons={persons}></Numbers>
+      <Numbers persons={persons} filter={filter}></Numbers>
     </div>
   )
 }
